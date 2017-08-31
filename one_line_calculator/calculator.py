97a432ce0ffbaa673cea0f5dd7e51b303e0352b0
David Capella <D_Capella@yahoo.com>
2017-08-31 00:55:46 -0500

    white space function as well as addition, multiply, division, subtraction, and power

--- one_line_calculator/calculator.py
+++ one_line_calculator/calculator.py
@@ -1,2 +1,56 @@
-def addition(a, b):
-    return a + b
+import re
+
+def addition(solution, number):
+    return solution + number
+
+def subtraction(solution, number):
+    return solution - number
+
+def multiplication(solution, number):
+    return solution * number
+
+def division(solution, number):
+    return solution / number
+
+def power(solution, number):
+    return solution ** number
+
+def expression():
+    expression = input(">> ")
+    expression = re.sub('\s+', '', expression)
+    return expression
+
+def get_selection(character, symbol, solution):
+    symbols = {
+        '+' : addition,
+        '-' : subtraction,
+        '/' : division,
+        '*' : multiplication,
+        'x' : multiplication,
+        'X' : multiplication,
+        '^' : power,
+        '**': power
+    }
+    # symbols['*', 'x', 'X'] = multiplication
+    symbols['^', '**'] = power
+    answer = symbols.get(symbol, lambda: "nothing")
+    return answer(solution, character)
+
+def calculate(expression):
+    count = 0
+    solution = 0
+    symbol = '+'
+
+    for character in expression:
+        try:
+            character = int(character)
+            solution = get_selection(character, symbol, solution)
+            count = 0
+        except Exception as e:
+            if count > 0:
+                symbol = '**'
+            else:
+                symbol = character
+            count += 1
+
+    return solution
